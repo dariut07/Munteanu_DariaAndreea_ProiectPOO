5cbddbd646e7c24b06b42183a84f47fd9d86d5a5
dariut07 <148126582+dariut07@users.noreply.github.com>
2023-11-11 20:26:37 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -94,39 +94,51 @@ public:
 	}
 	//operatori
 	Spital& operator=(const Spital& spital) {      //1.1
-	   if (this != &spital) {
-		   this->nume = spital.nume;
-		   this->nrPaturi = spital.nrPaturi;
-		   this->nrPacienti = spital.nrPacienti;
+		if (this != &spital) {
+			this->nume = spital.nume;
+			this->nrPaturi = spital.nrPaturi;
+			this->nrPacienti = spital.nrPacienti;
 
-		   if (this->localitate != NULL) {
-			   delete[]this->localitate;
-		   }                                          
+			if (this->localitate != NULL) {
+				delete[]this->localitate;
+			}
 
-		   this->localitate = new char[strlen(spital.localitate)+1];
-		   strcpy_s(this->localitate, strlen(spital.localitate) + 1, spital.localitate);
+			this->localitate = new char[strlen(spital.localitate) + 1];
+			strcpy_s(this->localitate, strlen(spital.localitate) + 1, spital.localitate);
 
-		   return *this;
-	   }
-   }
-	 friend ostream& operator<<(ostream& scrie, const Spital& s) {  //1.2
-        scrie << "Nume:" << s.nume << endl;
-		scrie<<"An infiintare:"<<s.anInfiintare<<endl;
-        scrie << "Numar paturi:" << s.nrPaturi << endl;
-        scrie<<"Numar pacienti:"<<s.nrPacienti<<endl;
-        scrie << "Numar sectii ATI" << s.nrSectiiATI << endl;
-        cout << "Localitate: " << s.localitate << endl;
-        cout << endl << endl;
-        return scrie;
-    }
-	  Spital operator++() {    //preincrementare   //1.3
-        this->nrPaturi += 7;
-        return *this;
-    }
-	  Spital operator--() {      //1.4    
-		  this->nrPacienti -= 5;
-		  return *this;
-	  }
+			return *this;
+		}
+	}
+	friend ostream& operator<<(ostream& scrie, const Spital& s) {  //1.2
+		scrie << "Nume:" << s.nume << endl;
+		scrie << "An infiintare:" << s.anInfiintare << endl;
+		scrie << "Numar paturi:" << s.nrPaturi << endl;
+		scrie << "Numar pacienti:" << s.nrPacienti << endl;
+		scrie << "Numar sectii ATI" << s.nrSectiiATI << endl;
+		cout << "Localitate: " << s.localitate << endl;
+		cout << endl << endl;
+		return scrie;
+	}
+	Spital operator++() {    //preincrementare   //1.3
+		this->nrPaturi += 7;
+		return *this;
+	}
+	Spital operator--() {      //1.4    
+		this->nrPacienti -= 5;
+		return *this;
+	}
+	friend istream& operator>>(istream& citeste, Spital& s) {   
+		cout << "Nume:"; citeste >> s.nume;
+		cout << "Numar paturi:"; citeste >> s.nrPaturi;
+		cout << "Numar pacienti:"; citeste >> s.nrPacienti;
+		cout << "Localitate:"; 
+		if (s.localitate != NULL) {
+			delete[]s.localitate;
+		}
+		s.localitate = new char[strlen(s.localitate) + 1];
+		citeste >> s.localitate;
+		return citeste;
+	}
 	//destructor
 	~Spital() {
 		if (this->localitate != NULL)
@@ -230,6 +242,16 @@ public:
 		cout << endl;
 		cout << endl;
 	}
+	echipamentMedical() :nrOperatori(2) {
+		this->nume = "Aparat de ventilatie";
+		this->specializare = new char[strlen("Urgente") + 1];
+		strcpy_s(this->specializare, strlen("Urgente") + 1, "Urgente");
+		this->pret = 30000;
+		this->aniUtilizare = 2;
+		this->estePornit = 0;
+		this->nrDefectiuni = 0;
+
+	}
 	echipamentMedical(string nume, const char* specializare, int aniUtilizare, const int nrOp) :nrOperatori(nrOp) {
 		this->nume = nume;
 		this->specializare = new char[strlen(specializare) + 1];
@@ -305,35 +327,46 @@ public:
 		}
 	}
 	friend istream& operator>>(istream& citeste, echipamentMedical& e) {   //2.2
-        cout << "Nume:"; citeste >> e.nume;
-		cout<<"Specializare:";
-		if(e.specializare!=NULL){
-		delete[]e.specializare;
+		cout << "Nume:"; citeste >> e.nume;
+		cout << "Specializare:";
+		if (e.specializare != NULL) {
+			delete[]e.specializare;
 		}
-		e.specializare=new char[strlen(e.specializare)+1];
-		citeste>>e.specializare;
-        cout << "Pret:"; citeste >> e.pret;
-		cout<<"Ani de utilizare:"; citeste>>e.aniUtilizare;
-        cout<<"Este pornit:"; citeste>>e.estePornit;
-cout<<"Numar defectiuni:"; citeste>>e.nrDefectiuni;
-        return citeste;
-    }
-	 echipamentMedical operator++(int) {  //2.3
-        echipamentMedical temporar = *this;
-        this->aniUtilizare += 3;
-        return temporar;
-    }
-	
-	 bool operator>(echipamentMedical e) {  //2.4
-		 return this->pret > e.pret;
-	 }
+		e.specializare = new char[strlen(e.specializare) + 1];
+		citeste >> e.specializare;
+		cout << "Pret:"; citeste >> e.pret;
+		cout << "Ani de utilizare:"; citeste >> e.aniUtilizare;
+		cout << "Este pornit:"; citeste >> e.estePornit;
+		cout << "Numar defectiuni:"; citeste >> e.nrDefectiuni;
+		return citeste;
+	}
+	friend ostream& operator<<(ostream& scrie , echipamentMedical& e) {
+		scrie << "Nume:"<< e.nume<< endl;
+		scrie << "Specializare:" << e.specializare << endl;
+		scrie << "Pret:" << e.pret << endl;
+		scrie << "Ani de utilizare:" << e.aniUtilizare << endl;
+		scrie << "Perioada de garantie" << e.perioadaGarantie << " ani" << endl;
+		scrie << "Numar operatori:" << e.nrOperatori<<endl;
+		scrie << "Este pornit:" << e.estePornit << endl;
+		scrie << "Numar defectiuni:" << e.nrDefectiuni << endl;
+		return scrie;
+	}
+	echipamentMedical operator++(int) {  //2.3
+		echipamentMedical temporar = *this;
+		this->aniUtilizare += 3;
+		return temporar;
+	}
+
+	bool operator>(echipamentMedical e) {  //2.4
+		return this->pret > e.pret;
+	}
 	~echipamentMedical() {
 		if (this->specializare != NULL)
 		{
 			delete[]this->specializare;
 		}
 	}
-	
+
 	//fct de return var statica
 	static int getPerioadaGarantie() {
 		return perioadaGarantie;
@@ -419,7 +452,15 @@ public:
 		cout << endl;
 		cout << endl;
 	}
-
+	Medicament() :termenValabilitate(6) {
+		this->denumire = new char[strlen("Aerius") + 1];
+		strcpy_s(this->denumire, strlen("Aerius") + 1, "Aerius");
+		this->modDeAdministrare = "oral";
+		this->pretIntreg = 34;
+		this->reducere = 0.3;
+		this->prescriptieMedicala = 0;
+		this->nrFarmaciiPartenere = 12;
+	}
 	Medicament(const char* denumire, float pretIntreg, bool prescriptieMedicala, int nrFarmaciiPartenere) :termenValabilitate(6) {
 		this->denumire = new char[strlen(denumire) + 1];
 		strcpy_s(this->denumire, strlen(denumire) + 1, denumire);
@@ -475,20 +516,48 @@ public:
 
 	}
 	bool operator<(Medicament m) {      //3.2
-	return this->pretIntreg < m.pretIntreg;
+		return this->pretIntreg < m.pretIntreg;
 	}
 	Medicament operator++(int) {       //3.3
-		Medicament temporar=*this;
+		Medicament temporar = *this;
 		this->reducere += 0.05;
 		return temporar;
 
 	}
-	
-	 bool operator!=(Medicament m) {    //3.4
-		 return this->nrFarmaciiPartenere != m.nrFarmaciiPartenere;
-	 }
-	
-	
+
+	bool operator!=(Medicament m) {    //3.4
+		return this->nrFarmaciiPartenere != m.nrFarmaciiPartenere;
+	}
+	//operator citire de la tastatura
+	friend istream& operator>>(istream& citire, Medicament& m) {
+		cout << "Denumire:";
+		if (m.denumire != NULL) {
+			delete[]m.denumire;
+
+		}
+		m.denumire = new char[strlen(m.denumire) + 1];
+		citire >> m.denumire;
+		cout << "Mod de administrare:"; citire >> m.modDeAdministrare;
+		cout << "Pret intreg:"; citire >> m.pretIntreg;
+		cout << "Reducere:"; citire >> m.reducere;
+		cout << "Prescriptie medicala:"; citire >> m.prescriptieMedicala;
+		cout << "Numar farmacii partenere:"; citire >> m.nrFarmaciiPartenere;
+		return citire;
+	}
+	friend ostream& operator<<(ostream& scrie, const Medicament& m) {
+		scrie << "Denumire:" << m.denumire << endl;
+		scrie << "Mod de administrare:" << m.modDeAdministrare << endl;
+		scrie << "Pret intreg:" << m.reducere<<endl;
+		scrie << "Reducere:" << m.reducere << endl;
+		scrie << "TVA:" << m.TVA<<endl;
+		scrie << "Prescriptie medicala:" << m.prescriptieMedicala << endl;
+		scrie << "Numar farmacii partenere:" << m.nrFarmaciiPartenere << endl;
+		scrie << "Termen de valabiliate:" << m.termenValabilitate << " luni" << endl;
+		return scrie;
+
+
+	}
+
 	~Medicament() {
 		if (this->denumire != NULL)
 		{
@@ -511,14 +580,14 @@ void pretRedus(Medicament x) {
 }
 void main() {
 	Spital s1;
-	cout << "Obiectul s1:"<<endl;
+	cout << "Obiectul s1:" << endl;
 	s1.afisare();
 	paturiNecesare(s1);
 	Spital s2("Ioan Lascar", 2009, 12, 34, "Comanesti");
 	Spital s3(1990, 12, 88);
-	cout << "Obiectul s2:"<<endl;
+	cout << "Obiectul s2:" << endl;
 	s2.afisare();
-	cout << "Obiectul s3:"<<endl;
+	cout << "Obiectul s3:" << endl;
 	s3.afisare();
 	cout << "******************************************************";
 	echipamentMedical e1("EKG", "cardiologie", 5, 1);
@@ -544,7 +613,7 @@ void main() {
 	cout << "#########################################################";
 	cout << "\nVERIFICARE GETTERI SI SETTERI:";
 	cout << "\nPentru clasa Spital:" << endl;
-	cout << "GETTERI:"<<endl;
+	cout << "GETTERI:" << endl;
 	cout << s1.getNume() << " |" << s1.getAnInfiintare() << " |" << s1.getNrPaturi() << " paturi | ";
 	cout << s1.getNrPacienti() << " pacienti | " << s1.getSectiiATI() << " sectii ATI | " << s1.getLocalitate() << endl;
 	cout << "SETTERI:" << endl;
@@ -594,17 +663,17 @@ void main() {
 	Spital s5;
 	cout << "\nObiectul s5:" << endl;
 	s5.afisare();
-	cout <<"Dupa utilizrea operatorului = noul s5 va fi:"<< endl;
+	cout << "Dupa utilizrea operatorului = noul s5 va fi:" << endl;
 	s5 = s3;
 	s5.afisare();
 	echipamentMedical e4 = e1;
 	cout << "\nObiectul e4 realizat pe baza constructorului de copiere:" << endl;
 	e4.afisare();
-	echipamentMedical e5("Glucometru","Diabetologie",6,2);
+	echipamentMedical e5("Glucometru", "Diabetologie", 6, 2);
 	cout << "\nObiectul e5:" << endl;
 	e5.afisare();
 	e5 = e2;
-	cout << "Dupa utilizarea operatorului = obiectul e5 este:"<<endl;
+	cout << "Dupa utilizarea operatorului = obiectul e5 este:" << endl;
 	e5.afisare();
 	cout << endl;
 	Medicament m4 = m3;
@@ -617,7 +686,7 @@ void main() {
 	cout << "\nDupa utilizarea operatorului = obiectul m5 este:" << endl;
 	m5.afisare();
 	cout << "Verificare operatori,exceptand cei de atribuire:" << endl;
-	cout << "Operatorului <<:"<<endl;
+	cout << "Operatorului <<:" << endl;
 	cout << s1;
 	Spital s6;
 	cout << "Obiectul s6:" << endl;
@@ -630,24 +699,24 @@ void main() {
 	cout << "Dupa aplicarea operatorilor obiectul s7 este:" << endl;
 	s7.afisare();
 	echipamentMedical e6(1);
-	cout << "Citire echipament medical"<<endl;
+	cout << "Citire echipament medical" << endl;
 	cin >> e6;
 	e6.afisare();
 	if (e1 > e2) {
-		cout << "Echipamentul medical " << e1.getNume() << " este mai scump decat" << e2.getNume()<<endl;
+		cout << "Echipamentul medical " << e1.getNume() << " este mai scump decat" << e2.getNume() << endl;
 	}
 	else
 	{
-		cout << "Echipamentul medical " << e1.getNume() << " este mai ieftin sau la fel de scump ca " << e2.getNume()<<endl;
+		cout << "Echipamentul medical " << e1.getNume() << " este mai ieftin sau la fel de scump ca " << e2.getNume() << endl;
 	}
 	if (m2 < m4) {
-		cout << "Medicamentul "<<m2.getDenumire() << " este mai ieftin decat "<< m4.getDenumire()<<endl;
+		cout << "Medicamentul " << m2.getDenumire() << " este mai ieftin decat " << m4.getDenumire() << endl;
 	}
 	else
 	{
-		cout << "Medicamentul " << m2.getDenumire() << " este mai scump sau la fel de scump ca " << m4.getDenumire()<<endl;
+		cout << "Medicamentul " << m2.getDenumire() << " este mai scump sau la fel de scump ca " << m4.getDenumire() << endl;
 	}
-	
+
 	echipamentMedical e7(0);
 	cout << "Obiectul e7:";
 	e7.afisare();
@@ -672,6 +741,53 @@ void main() {
 	{
 		cout << "Medicamentul " << m3.getDenumire() << " este la acelasi numar de farmacii precum medicamentul " << m2.getDenumire();
 	}
-	
-	
+	Spital s[5];
+	int i;
+	cout << "Citire spitale:" << endl;
+	for (i = 0; i < 2; i++) {
+		cin >> s[i];
+	}
+	cout << "Afisare spitale:" << endl;
+	for (i = 0; i < 2; i++) {
+		cout<< s[i];
+		cout << "**********************************************************" << endl;
+	}
+	cout << "\nCitire echipament medical:" << endl;
+	echipamentMedical e[5];
+	for (i = 0; i < 2; i++) {
+		cin >> e[i];
+
+	}
+	cout << "\nAfisare echipament medical:"<<endl;
+	for (i = 0; i < 2; i++) {
+		cout << e[i];
+		cout << "**************************************************************" << endl;
+		
+
+	}
+	cout << "Citire medicamente:" << endl;
+	Medicament m[5];
+	for (i = 0; i < 2; i++) {
+		cin >> m[i];
+	}
+	cout << "\nAfisare medicamente:"<<endl;
+	for (i = 0; i < 2; i++) {
+		cout<< m[i];
+		cout << "*********************************************************************"<<endl;
+	}
+cout << "Citire matrice cu medicamente:" << endl;
+	Medicament md[2][2];
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++)
+			cin >> md[i][j];
+	}
+	cout << "Afisare matrice cu medicamente:" << endl;
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++)
+		{
+			cout << md[i][j];
+			cout << "************************************************************************" << endl;
+		}
+	}
+
 }
